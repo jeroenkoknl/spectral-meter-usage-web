c0f3c6521d050e56e0605195010f566435578309
Jeroen Kok <jeroen.kok@moneyou.nl>
2019-07-20 15:26:53 +0200

    implemented simple web api

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
 const services = require('./meter-usage_grpc_pb');
 const messages = require('./meter-usage_pb');
 const grpc = require('grpc');
-// import * as messages from './meter-usage_pb';
+var express = require("express");
 
-
-function main() {
-    const client = new services.MeterUsageClient('localhost:50051',
+const client = new services.MeterUsageClient('localhost:50051',
                                             grpc.credentials.createInsecure());
 
+function runGetMeasurements(callback) {
+    const measurements = [];
     const call = client.getMeasurements( new messages.GetMeasurementsRequest());
     call.on('data', function(measurement) {
         console.log('Found measurement:', measurement.getTimestamp(), measurement.getMetervalue())
+        measurements.push(measurement);
+    });
+    call.on('end', () => callback(null, measurements));
+    call.on('error', callback);
+}
+
+function main() {
+
+    const app = express();
+
+    app.get('/api/measurements', (req, res, next) => {
+        runGetMeasurements((err, measurements) => {
+            if (err) {
+                next(err);
+            } else {
+                res.json(measurements.map((measurement) => ({ 
+                    timestamp: measurement.getTimestamp(),
+                    metervalue: measurement.getMetervalue(),
+                })));
+            }
+        })
+    });
+    app.listen(3000, () => {
+        console.log("Server running on port 3000");
     });
 }
 
